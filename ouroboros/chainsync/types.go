a6af7c9a4e7042ebc49a8f6cc658fa5251c8bd73
Jenita <jkawan@blinklabs.io>
2026-02-28 17:40:42 -0600

    fix: addressed review comment
    Signed-off-by: Jenita <jkawan@blinklabs.io>

--- ouroboros/chainsync/types.go
+++ ouroboros/chainsync/types.go
@@ -749,7 +749,7 @@ type OgmiosMetadatumMap struct {
 }
 
 func GetMetadataDatums(datums map[string][]byte) ([][]byte, error) {
-	datumBytes := make([][]byte, 0)
+	datumBytes := make([][]byte, 0, len(datums))
 	for _, datum := range datums {
 		datumBytes = append(datumBytes, datum)
 	}
